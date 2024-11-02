59a973e173f1ef1e87728a80d89fdfb8108ddc14
EliasHdzR <2230200@upv.edu.mx>
2024-11-01 23:07:40 -0600

    buscaminas terminado

--- practica03/js/index.js
+++ practica03/js/index.js
@@ -1,9 +1,46 @@
-const inputTamano = document.querySelector("#input-filas");
+const inputFilas = document.querySelector("#input-filas");
 const inputColumnas = document.querySelector("#input-columnas");
+const inputMinas = document.querySelector("#input-minas");
 const btnTamano = document.querySelector("#btn-tamano");
+btnTamano.addEventListener("click", modoPersonalizado);
+
+inputFilas.value = "";
+inputColumnas.value = "";
+inputMinas.value = "";
+
+function modoPersonalizado() {
+    let filasPers = parseInt(inputFilas.value.trim(), 10);
+    const columnasPers = parseInt(inputColumnas.value.trim(), 10);
+    const minasPers = parseInt(inputMinas.value.trim(), 10);
+
+    if (isNaN(filasPers) || filasPers < 5) {
+        inputFilas.value = "";
+        inputFilas.focus();
+        return;
+    }
+
+    if (isNaN(columnasPers) || columnasPers < 5) {
+        inputColumnas.value = "";
+        inputColumnas.focus();
+        return;
+    }
+
+    if (isNaN(minasPers) || minasPers < 5 || minasPers > filasPers * columnasPers - 1) {
+        inputMinas.value = "";
+        inputMinas.focus();
+        return;
+    }
+
+    iniciarJuego(filasPers, columnasPers, minasPers);
+}
 
 const divEleccion = document.querySelector(".eleccion-container");
+const divAviso = document.querySelector(".aviso");
+const aviso = document.querySelector("#h-aviso");
 const divJuego = document.querySelector(".juego-container");
+const btnReiniciar = document.querySelector("#btn-reiniciar")
+btnReiniciar.addEventListener("click", function (){window.location.reload();})
+const infoH = document.querySelector("#h-info");
 const tablero = document.querySelector("#tablero");
 
 // dificultades predeterminadas
@@ -27,6 +64,13 @@ let primerTurno = true;
 let listaMinas = [];
 let listaBotones = [];
 let listaCuadriculasLibres = [];
+let cuadriculasreveladas = 0;
+
+// todos los tipos son como hijos de mi clase main Espacio
+function Espacio(posFila, posColumna){
+    this.posFila = posFila;
+    this.posColumna = posColumna;
+}
 
 function Boton(posFila, posColumna, idBoton){
     this.posFila = posFila;
@@ -44,6 +88,7 @@ function CuadriculaLibre(posFila, posColumna, numero){
     this.posFila = posFila;
     this.posColumna = posColumna;
     this.numero = numero;
+    this.revelada = false;
 }
 
 function iniciarJuego(filas, columnas, minas){
@@ -51,6 +96,8 @@ function iniciarJuego(filas, columnas, minas){
     cantColumnas = columnas;
     cantMinas = minas;
 
+    infoH.innerHTML = `Cantidad de Minas: ${cantMinas}`;
+    infoH.setAttribute("style","display:block;")
     divEleccion.setAttribute('style','display:none');
     divJuego.setAttribute('style','display:block');
     crearTablero();
@@ -79,21 +126,33 @@ function crearTablero(){
 }
 
 function accionBotonesTablero(idBoton){
+    let boton;
+    for(let i = 0; i < listaBotones.length; i++){
+        const botonTemp = listaBotones[i];
+        if(botonTemp.idBoton == idBoton){
+            boton = botonTemp;
+        }
+    }
+
     if(primerTurno){
         primerTurno = false;
-        generarMinas(idBoton);
-        console.log(listaMinas);
+        generarMinas(boton);
+        calcularNumeros();
     }
+
+    const espacio = new Espacio(boton.posFila, boton.posColumna);
+    develarContenidoCuadricula(espacio);
 }
 
-function generarMinas(idBoton){
+function generarMinas(boton){
+    const idBoton = boton.idBoton;
     for(let i = 0; i < cantMinas; i++){
         let yaExiste = false;
-        const randomFila = Math.random() * cantFilas;
-        const randomColumna = Math.random() * cantColumnas;
+        const randomFila = Math.floor(Math.random() * cantFilas);
+        const randomColumna = Math.floor(Math.random() * cantColumnas);
 
         // no se pede generar una mina en el boton que se clickeó
-        if(`btn-${randomFila}-${randomColumna}` === idBoton){
+        if(`btn-${randomFila}-${randomColumna}` == idBoton){
             i--;
             continue;
         }
@@ -103,9 +162,9 @@ function generarMinas(idBoton){
 
         // para checar que no se generen 2 minas en el mismo espacio
         for(let j = 0; j < listaMinas.length; j++){
-            const minaExistente = listaMinas.at(j);
+            const minaExistente = listaMinas[j];
             if(!minaExistente) break;
-            if(minaExistente.botonAsociado === nuevaMina.botonAsociado){
+            if(minaExistente.botonAsociado == nuevaMina.botonAsociado){
                 yaExiste = true;
             }
         }
@@ -125,8 +184,8 @@ function generarMinas(idBoton){
             let esLibre = true;
 
             for(let h = 0; h < listaMinas.length; h++){
-                const mina = listaMinas.at(h);
-                if(mina.posFila === cuadriculaLibre.posFila && mina.posColumna === cuadriculaLibre.posColumna){
+                const mina = listaMinas[h];
+                if(mina.posFila == cuadriculaLibre.posFila && mina.posColumna == cuadriculaLibre.posColumna){
                     esLibre = false;
                     break;
                 }
@@ -140,18 +199,226 @@ function generarMinas(idBoton){
 }
 
 function calcularNumeros(){
+    for(let i = 0; i < listaMinas.length; i++){
+        const mina = listaMinas[i];
+        const espaciosAdyacentes = getEspaciosAdyacentes(mina);
+        for(let j = 0; j < espaciosAdyacentes.length; j++){
+            const espacioAdyacente = espaciosAdyacentes[j];
+            for(let k = 0; k < listaCuadriculasLibres.length; k++){
+                const cuadriculaLibre = listaCuadriculasLibres[k];
+                if(espacioAdyacente.posFila == cuadriculaLibre.posFila && espacioAdyacente.posColumna == cuadriculaLibre.posColumna){
+                    cuadriculaLibre.numero++;
+                }
+            }
+        }
+    }
+}
 
+function develarContenidoCuadricula(espacio){
+    const fila = espacio.posFila;
+    const columna = espacio.posColumna;
+
+    const tipoCuadricula = checarTipo(fila, columna);
+
+    if(tipoCuadricula instanceof CuadriculaLibre) {
+        const cuadriculaLibre = tipoCuadricula;
+        const btnElegido = document.querySelector(`#btn-${fila}-${columna}`);
+        tipoCuadricula.revelada = true;
+        cuadriculasreveladas++;
+
+        const tdContenedor = btnElegido.parentNode;
+
+        if(cuadriculaLibre.numero != 0){
+            tdContenedor.innerHTML = cuadriculaLibre.numero;
+        } else {
+            tdContenedor.innerHTML = "";
+        }
+
+        if(cuadriculasreveladas == listaCuadriculasLibres.length){
+            ganarJuego();
+            return;
+        }
+
+        if(cuadriculaLibre.numero == 0){
+            const espaciosAdyacentes = getEspaciosAdyacentes(espacio);
+            for(let i = 0; i < espaciosAdyacentes.length; i++){
+                const espacioAdyacente = espaciosAdyacentes[i];
+                const tipoCuadriculaAdy = checarTipo(espacioAdyacente.posFila, espacioAdyacente.posColumna);
+
+                if (tipoCuadriculaAdy instanceof CuadriculaLibre && !tipoCuadriculaAdy.revelada) {
+                    develarContenidoCuadricula(tipoCuadriculaAdy);
+                }
+            }
+        }
+
+    }
+
+    if(tipoCuadricula instanceof Mina){
+        for(let i = 0; i < listaMinas.length; i++) {
+            const mina = listaMinas[i];
+            const btnElegido = document.querySelector(`#btn-${mina.posFila}-${mina.posColumna}`);
+            const tdContenedor = btnElegido.parentNode;
+            tdContenedor.innerHTML = "*";
+        }
+        terminarJuego();
+    }
+}
+
+function terminarJuego(){
+    divAviso.setAttribute("style", "display: block;")
+    aviso.innerHTML = "PARTIDA TERMINADA";
+    for(let i = 0; i < listaBotones.length; i++){
+        const boton = document.querySelector(`#${listaBotones[i].idBoton}`);
+        if(boton){
+            boton.setAttribute("disabled","true");
+        }
+    }
+}
+
+function ganarJuego(){
+    divAviso.setAttribute("style", "display: block;")
+    aviso.innerHTML = "¡GANASTE!";
+
+    for(let i = 0; i < listaBotones.length; i++){
+        const boton = document.querySelector(`#${listaBotones[i].idBoton}`);
+        if(boton){
+            boton.setAttribute("disabled","true");
+        }
+    }
 }
 
 /**
  * Devuelve todos los espacios adyacentes de un cuadrado dado, el cuadrado puede ser un boton o una mina
  */
-function getEspaciosLibresAdyacentes(espacio){
+function getEspaciosAdyacentes(espacio){
     const fila = espacio.posFila;
     const columna = espacio.posColumna;
-    let arrayCuadriculasLibresAdyacentes = [];
+    let arrayEspaciosAdyacentes = [];
+    let yaUbicado = false
+    //para no andar haciéndolo manualmente
+    let coordenadas;
 
-    if(fila === 0 && columna === 0){
-        arrayAdyacentes.push()
+    // checamos si es una de las 4 esquinas del tablero
+    if(!yaUbicado && fila == 0 && columna == 0){
+        yaUbicado = true;
+        coordenadas = [
+            [0,1],
+            [1,0],
+            [1,1]
+        ];
+    }
+
+    if(!yaUbicado && fila == 0 && columna == cantColumnas - 1){
+        yaUbicado = true;
+        coordenadas = [
+            [0, columna - 1],
+            [1, columna - 1],
+            [1, columna]
+        ]
+    }
+
+    if(!yaUbicado && fila == cantFilas - 1 && columna == 0){
+        yaUbicado = true
+        coordenadas = [
+            [fila - 1, 0],
+            [fila - 1, 1],
+            [fila, 1]
+        ];
+    }
+
+    if(!yaUbicado && fila == cantFilas - 1 && columna == cantColumnas - 1){
+        yaUbicado = true;
+        coordenadas = [
+            [fila - 1, columna],
+            [fila - 1, columna - 1],
+            [fila, columna - 1]
+        ]
+    }
+
+    // checamos si una de sus coordenadas está pegada a un borde
+    if(!yaUbicado && columna > 0 && columna < cantColumnas - 1 && fila == 0){
+        yaUbicado = true;
+        coordenadas = [
+            [fila, columna - 1],
+            [fila + 1, columna - 1],
+            [fila + 1, columna],
+            [fila + 1, columna + 1],
+            [fila, columna + 1]
+        ]
+    }
+
+    if(!yaUbicado && columna > 0 && columna < cantColumnas - 1 && fila == cantFilas - 1){
+        yaUbicado = true;
+        coordenadas = [
+            [fila - 1, columna - 1],
+            [fila - 1, columna],
+            [fila - 1, columna + 1],
+            [fila, columna - 1],
+            [fila, columna + 1]
+        ];
+    }
+
+    if(!yaUbicado && fila > 0 && fila < cantFilas - 1 && columna == 0){
+        yaUbicado = true;
+        coordenadas = [
+            [fila - 1, columna],
+            [fila - 1, columna + 1],
+            [fila, columna + 1],
+            [fila + 1, columna + 1],
+            [fila + 1, columna]
+        ];
+    }
+
+    if(!yaUbicado && fila > 0 && fila < cantFilas - 1 && columna == cantColumnas - 1){
+        yaUbicado = true;
+        coordenadas = [
+            [fila - 1, columna],
+            [fila - 1, columna - 1],
+            [fila, columna - 1],
+            [fila + 1, columna - 1],
+            [fila + 1, columna]
+        ];
+    }
+
+    // cuando está separado de las orillas
+    if(!yaUbicado){
+        coordenadas = [
+            [fila - 1, columna - 1],
+            [fila - 1, columna],
+            [fila - 1, columna + 1],
+            [fila, columna - 1],
+            [fila, columna + 1],
+            [fila + 1, columna - 1],
+            [fila + 1, columna],
+            [fila + 1, columna + 1]
+        ];
+    }
+
+    coordenadas.forEach(([filaAr, columnaAr]) => {
+        arrayEspaciosAdyacentes.push(new Espacio(filaAr, columnaAr));
+    });
+
+    return arrayEspaciosAdyacentes;
+}
+
+/**
+ * Recibe unas coordenadas como parametro y devuelve su tipo (mina o cuadriculalibre)
+ * @returns {*}
+ * @param filaEspacio
+ * @param columnaEspacio
+ */
+function checarTipo(filaEspacio, columnaEspacio){
+    for(let i = 0; i < listaMinas.length; i++){
+        let mina = listaMinas[i];
+        if(mina.posFila == filaEspacio && mina.posColumna == columnaEspacio){
+            return mina;
+        }
+    }
+
+    for(let i = 0; i < listaCuadriculasLibres.length; i++){
+        let cuadriculaLibre = listaCuadriculasLibres[i];
+        if(cuadriculaLibre.posFila == filaEspacio && cuadriculaLibre.posColumna == columnaEspacio){
+            return cuadriculaLibre;
+        }
     }
 }
